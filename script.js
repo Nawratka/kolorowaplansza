545672714003324e0caf1182634a12daac3e7c38
Kamila <kamilanawrat@gmail.com>
2023-09-15 00:35:44 +0200

    complete

---
+++ script.js
@@ -0,0 +1,57 @@
+const box = document.querySelector('.box');
+const speedBtns = document.querySelectorAll('[data-setting="speed"]');
+const colorBtns = document.querySelectorAll('[data-setting="color"]');
+const slider = document.getElementById('slider');
+const sliderInfo = document.querySelector('.slider-info');
+
+const squares = 310;
+let sliderValue = 70;
+let range = 360;
+
+const createSquare = speed => {
+    box.innerHTML = '';
+	for (let i = 0; i < squares; i++) {
+		const square = document.createElement('div');
+		square.classList.add('square');
+        square.style.transitionDuration = speed;
+		square.addEventListener('mouseover', () => setColor(square));
+		box.appendChild(square);
+        square.addEventListener('mouseout', () => {deleteColor(square)})
+	}
+};
+
+const setColor = (square) => {
+	let h;
+	if (range === 360) {
+		h = Math.floor(Math.random() * 360)
+	} else {
+		h = Math.floor(Math.random() * 60) + range
+	}
+	const s = slider.value + '%';
+	const l = '50%';
+
+	square.style.backgroundColor = `hsl(${h},${s},${l})`
+};
+
+const deleteColor = square => {
+    square.style.backgroundColor = 'transparent';
+}
+
+function handleSpeed () {
+    const newSpeed = this.dataset.speed + 's';
+    createSquare(newSpeed);
+}
+
+function handleColorRange () {
+    range = parseInt(this.dataset.colorRange)
+}
+
+const showSliderInfo = () => {
+    sliderValue = slider.value;
+    sliderInfo.textContent = sliderValue;
+}
+
+speedBtns.forEach(btn => btn.addEventListener('click', handleSpeed));
+colorBtns.forEach(btn => btn.addEventListener('click', handleColorRange));
+slider.addEventListener('mousemove', showSliderInfo)
+createSquare();
